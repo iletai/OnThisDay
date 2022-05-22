17573e207b37a466a3e37b92429d0d5d74950d15
Lê Quang Trọng Tài <iletai@hotmail.com>
2022-05-22 21:44:06 +0700

    + Add FirebaseGoogleInfo.plist

--- On This Day/CustomComponent/Button/ButtonCustom.swift
+++ On This Day/CustomComponent/Button/ButtonCustom.swift
@@ -8,13 +8,7 @@
 import UIKit
 
 class ButtonCustom: UIButton {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    override class func awakeFromNib() {
+        super.awakeFromNib()
     }
-    */
-
 }
