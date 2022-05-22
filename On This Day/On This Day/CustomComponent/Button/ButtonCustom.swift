1492c9d970f1d815492f29e560762873cfd01c65
Tàinè <iletai@hotmail.com>
2022-05-22 18:16:02 +0700

    Init project Xcode

---
+++ On This Day/On This Day/CustomComponent/Button/ButtonCustom.swift
@@ -0,0 +1,20 @@
+//
+//  ButtonCustom.swift
+//  On This Day
+//
+//  Created by Lê Quang Trọng Tài on 5/22/22.
+//
+
+import UIKit
+
+class ButtonCustom: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ On This Day/On This Day/CustomComponent/TextFill/TextFiledCustom.swift
@@ -0,0 +1,16 @@
+//
+//  TextFiledCustom.swift
+//  On This Day
+//
+//  Created by Lê Quang Trọng Tài on 5/22/22.
+//
+
+import UIKit
+
+class TextFiledCustom: UITextField {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        layer.cornerRadius = 55
+    }
+}
